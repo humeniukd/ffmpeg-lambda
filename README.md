763a488f1649a9446280738f61f9695a420f4c25
Dmitry Humeniuk <dmitry.gumenyuk@gmail.com>
2020-11-07 16:29:22 +0100

    Add files via upload
    https://patchwork.ffmpeg.org/project/ffmpeg/patch/20190107143115.101095-1-dhumeniuk@google.com

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # wf-lambda
+Static FFmpeg Build for AWS Lambda runtime https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
 AWS Lambda python FFmpeg wrapper to work with S3 and SQS
-Custom FFmpeg release optimized for Amazon Linux AMI amzn-ami-hvm-2018.03.0.20181116-x86_64
+Custom FFmpeg release optimized for Amazon Linux AMI amzn-ami-hvm-2018.03.0.20181129-x86_64-gp2
+Just set index.py#handler() as S3 'Put' Event notification handler
 
--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from re import U, I, compile as recompile
 from os import remove, environ
 from math import floor
+import csv, json
 
 s3 = boto3.resource('s3')
 sqs = boto3.resource('sqs')
@@ -11,16 +12,16 @@ sqs = boto3.resource('sqs')
 HEIGHT = 140
 WIDTH = 1800
 
-IN_BUCKET_NAME = environ['IN_BUCKET_NAME']
 OUT_BUCKET_NAME = environ['OUT_BUCKET_NAME']
 
-WORK_DIR = environ['WORK_DIR']
+WORK_DIR = environ.get('WORK_DIR', '/tmp/')
 
 re_duration = recompile('Duration: (\d{2}):(\d{2}):(\d{2}).(\d{2})[^\d]*', U)
 re_freq = recompile('(\d+) Hz', U | I)
 re_position = recompile('out_time_ms=(\d+)\d{3}', U)
 
 delta = timedelta(seconds=2)
+
 
 def time2ms(s):
     hours = 3600000 * int(s.group(1))
@@ -29,44 +30,42 @@ def time2ms(s):
     ms = 10 * int(s.group(4))
     return hours + minutes + seconds + ms
 
+
 def ratio(position, duration):
     if not position or not duration:
         return 0
     percent = int(floor(100 * position / duration))
     return 100 if percent > 100 else percent
 
+
 class WfThread(object):
-    global IN_BUCKET_NAME, WORK_DIR, WIDTH, HEIGHT
+    global WORK_DIR, WIDTH, HEIGHT
     __SQSQueue = None
     __inBucket = None
 
     @property
     def SQSQueue(self):
         if None == self.__SQSQueue:
-            self.__SQSQueue = sqs.create_queue(QueueName=self.__key+'.fifo',
-                                               Attributes={'FifoQueue': 'true', 'ContentBasedDeduplication':'true'})
+            self.__SQSQueue = sqs.create_queue(QueueName=self.__key + '.fifo',
+                                               Attributes={'FifoQueue': 'true', 'ContentBasedDeduplication': 'true'})
         return self.__SQSQueue
 
-    @property
-    def inBucket(self):
-        if None == self.__inBucket:
-            self.__inBucket = s3.Bucket(IN_BUCKET_NAME)
-        return self.__inBucket
-
-    def __init__(self, key=None):
+    def __init__(self, key=None, bucket=None):
         self.__key = key
         self.__outFile = key + '.mp3'
-        self.__mJsonFile = key + '_m.json'
+        self.__csvFile = key + '.csv'
+        self.__jsonFile = key + '.json'
+        self.__inBucket = s3.Bucket(bucket)
         self.__ts = datetime.now()
 
     def __del__(self):
-        for fileName in [self.__mJsonFile, self.__key, self.__outFile]:
+        for fileName in [self.__csvFile, self.__key, self.__outFile]:
             remove(WORK_DIR + fileName)
-        self.inBucket.delete_objects(Delete={'Objects':[{'Key': self.__key}]})
+        self.__inBucket.delete_objects(Delete={'Objects': [{'Key': self.__key}]})
         print('Destruct %s', self.__key)
 
     def run(self):
-        for fn in [self.__download, self.__probe, self.__process, self.__upload]:
+        for fn in [ self.__download, self.__probe, self.__process, self.__convert, self.__upload]:
             if not fn():
                 self.__enqueue('{"key": "error"}')
                 break
@@ -96,15 +95,15 @@ class WfThread(object):
         return 0 == rc
 
     def __process(self):
-        spl = self.__duration*self.__freq/1000/WIDTH
+        spl = self.__duration * self.__freq / 1000 / WIDTH
         print('duration: %d, freq: %d, spl: %d', self.__duration, self.__freq, spl)
         process = Popen([
             './ffmpeg',
             '-i', WORK_DIR + self.__key,
             '-map', '0:0',
             '-progress', '/dev/stderr',
-            '-af', 'dumpwave=w=%d:h=%d:n=%d:f=%s' % (WIDTH, HEIGHT, spl, WORK_DIR + self.__mJsonFile),
-            WORK_DIR + self.__outFile
+            '-af', 'dumpwave=w=%d:n=%d:f=%s' % (WIDTH, spl, WORK_DIR + self.__csvFile),
+                  WORK_DIR + self.__outFile
         ], stdout=PIPE, stderr=PIPE, bufsize=1)
 
         ms = None
@@ -125,8 +124,8 @@ class WfThread(object):
             _percent = ratio(ms, self.__duration)
 
             if _percent != percent:
-                    percent = _percent
-                    self.__enqueue('{"type": "percent", "value": %(value)d}' % {'value': percent})
+                percent = _percent
+                self.__enqueue('{"type": "percent", "value": %(value)d}' % {'value': percent})
         isOk = 0 == rc
         if isOk:
             self.__enqueue('{"type": "percent", "value": %(value)d}' % {'value': 100}, True)
@@ -136,7 +135,7 @@ class WfThread(object):
         print('RC: %d', rc)
         return isOk
 
-    def __enqueue(self, msg, force = False):
+    def __enqueue(self, msg, force=False):
         now = datetime.now()
         if force or now - self.__ts > delta:
             self.__ts = now
@@ -144,27 +143,50 @@ class WfThread(object):
 
     def __download(self):
         try:
-            self.inBucket.download_file(self.__key, WORK_DIR + self.__key)
+            self.__inBucket.download_file(self.__key, WORK_DIR + self.__key)
         except Exception as e:
             print('Download failed %s', str(e))
             return False
         print('Downloaded %s', self.__key)
         return True
 
+    def __convert(self):
+        try:
+            data = []
+            with open(WORK_DIR + self.__csvFile) as csvFile:
+                csvReader = csv.DictReader(csvFile)
+                for v in csvReader.fieldnames:
+                    data.append(int(float(v)*HEIGHT))
+            with open(WORK_DIR + self.__jsonFile, 'w') as jsonFile:
+                jsonFile.write(json.dumps({
+                    'width': WIDTH,
+                    'height': HEIGHT,
+                    'samples': data
+                }))
+        except Exception as e:
+            print('Convert failed %s', str(e))
+            return False
+        print('Converted %s', self.__key)
+        return True
+
     def __upload(self):
         global WORK_DIR, OUT_BUCKET_NAME
         bucket = s3.Bucket(OUT_BUCKET_NAME)
-        for fileName in [self.__mJsonFile, self.__outFile]:
+        for fileName in [self.__jsonFile, self.__outFile]:
             try:
                 data = open(WORK_DIR + fileName, 'rb')
-                bucket.put_object(Key=fileName, Body=data, ACL='public-read', Metadata={'duration': str(self.__duration)})
+                bucket.put_object(Key=fileName, Body=data, ACL='public-read',
+                                  Metadata={'duration': str(self.__duration)})
             except Exception as e:
                 print('Upload failed: %s', str(e))
                 return False
             print('Uploaded %s', fileName)
         return True
 
+
 def handler(event, context):
     key = event['Records'][0]['s3']['object']['key']
-    WfThread(key).run()
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    WfThread(key, bucket).run()
     return 'ok'
+
